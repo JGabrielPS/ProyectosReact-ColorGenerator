b96cb23e3068fb94c833355638fd0d825a252922
Gabriel Perez <juan.gabriel.ps@gmail.com>
2021-03-17 13:21:41 -0300

    addition: added all the logic to render components and finish the project

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,8 +1,30 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React, { useState, useEffect } from "react";
 
-const SingleColor = () => {
-  return <h4>single color</h4>
-}
+const SingleColor = ({ rgb, weight, hex, index }) => {
+  const [alert, setAlert] = useState(false);
+  const value = rgb.join(",");
 
-export default SingleColor
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
+
+  return (
+    <article
+      className={`color ${index > 10 && "color-light"}`}
+      style={{ backgroundColor: `rgb(${value})` }}
+      onClick={(e) => {
+        setAlert(true);
+        navigator.clipboard.writeText(`#${hex}`);
+      }}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">#{hex}</p>
+      {alert && <p className="alert">copied to clipboard</p>}
+    </article>
+  );
+};
+
+export default SingleColor;
